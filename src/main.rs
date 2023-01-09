3e808798abee2976605bc67cb2b7b3ae97084325
jyuch <jyuch@users.noreply.github.com>
2023-01-09 18:48:52 +0900

    標準出力に大量にメッセージを出せるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod print;
 
-use crate::print::print_env;
+use crate::print::{print_env, print_message};
 use clap::Parser;
 use rand::random;
 use std::process::exit;
@@ -12,12 +12,23 @@ struct Cli {
     #[arg(long, short)]
     print_env: bool,
 
+    /// Print message num
+    #[arg(long, default_value = "0")]
+    print_message_num: i32,
+
+    /// Switch output stdout to stderr
+    #[arg(long)]
+    print_to_stderr: bool,
+
+    /// Enable crash emulation
     #[arg(long)]
     crash_emulation: bool,
 
+    /// Crash rate
     #[arg(long, default_value = "0.5")]
     crash_rate: f32,
 
+    /// Exit code on crash
     #[arg(long, default_value = "-1")]
     exit_code: i32,
 }
@@ -28,6 +39,8 @@ fn main() {
     if opt.print_env {
         print_env().unwrap();
     }
+
+    print_message(opt.print_to_stderr, opt.print_message_num).unwrap();
 
     if opt.crash_emulation && opt.crash_rate > random() {
         exit(opt.exit_code);
--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::io::{stderr, stdout, BufWriter, Write};
 
 pub(crate) fn print_env() -> std::io::Result<()> {
     let current_dir = env::current_dir()?;
@@ -17,6 +18,30 @@ pub(crate) fn print_env() -> std::io::Result<()> {
     let env_var = env::vars();
     for (key, value) in env_var {
         println!("{}={}", key, value);
+    }
+
+    Ok(())
+}
+
+pub(crate) fn print_message(print_to_stderr: bool, print_message_num: i32) -> std::io::Result<()> {
+    if print_to_stderr {
+        let out = stderr();
+        let out = out.lock();
+        let mut out = BufWriter::new(out);
+
+        for _ in 0..print_message_num {
+            out.write_all(b"The quick brown fox jumps over the lazy dog\r\n")?;
+        }
+        out.flush()?;
+    } else {
+        let out = stdout();
+        let out = out.lock();
+        let mut out = BufWriter::new(out);
+
+        for _ in 0..print_message_num {
+            out.write_all(b"The quick brown fox jumps over the lazy dog\r\n")?;
+        }
+        out.flush()?;
     }
 
     Ok(())
