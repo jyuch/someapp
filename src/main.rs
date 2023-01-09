3230fe916ad5242fff430b0224b66415c2252542
jyuch <jyuch@users.noreply.github.com>
2023-01-09 15:03:50 +0900

    指定した確率でクラッシュするように機能追加

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod print;
 
 use crate::print::print_env;
 use clap::Parser;
+use rand::random;
+use std::process::exit;
 
 #[derive(Parser, Debug)]
 #[clap(version, about)]
@@ -9,6 +11,15 @@ struct Cli {
     /// Print current directory
     #[arg(long, short)]
     print_env: bool,
+
+    #[arg(long)]
+    crash_emulation: bool,
+
+    #[arg(long, default_value = "0.5")]
+    crash_rate: f32,
+
+    #[arg(long, default_value = "-1")]
+    exit_code: i32,
 }
 
 fn main() {
@@ -16,5 +27,9 @@ fn main() {
 
     if opt.print_env {
         print_env().unwrap();
+    }
+
+    if opt.crash_emulation && opt.crash_rate > random() {
+        exit(opt.exit_code);
     }
 }
